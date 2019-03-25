0bbe209070fe31d9bad459a89ae1c8f75092105e
Prashanth <yvprashanth@gmail.com>
2019-03-24 18:47:21 -0700

    Todo Routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const cors = require('cors')
 const PORT = 4000
+const todoRoutes = express.Router();
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -15,8 +16,58 @@ mongoose.connect('mongodb://127.0.0.1:27017/todos', {
 const connection = monogoose.connection;
 
 
-connection.
+connection.once('open', function(){
+    console.log("Connection with DB Established");
+})
 
 app.listen(PORT, function(){
     console.log("Server is running on PORT:" + PORT);
 })
+
+app.use('/todos', todoRoutes);
+
+todoRoutes.route('/').get(function(req, res) {
+    Todo.find(function(err, todos) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(todos);
+        }
+    });
+});
+
+todoRoutes.route('/:id').get(function(req, res) {
+    let id = req.params.id;
+    Todo.findById(id, function(err, todo) {
+        res.json(todo);
+    });
+});
+
+todoRoutes.route('/add').post(function(req, res) {
+    let todo = new Todo(req.body);
+    todo.save()
+        .then(todo => {
+            res.status(200).json({'todo': 'todo added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('adding new todo failed');
+        });
+});
+
+todoRoutes.route('/update/:id').post(function(req, res) {
+    Todo.findById(req.params.id, function(err, todo) {
+        if (!todo)
+            res.status(404).send("data is not found");
+        else
+            todo.todo_description = req.body.todo_description;
+            todo.todo_responsible = req.body.todo_responsible;
+            todo.todo_priority = req.body.todo_priority;
+            todo.todo_completed = req.body.todo_completed;
+            todo.save().then(todo => {
+                res.json('Todo updated!');
+            })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            });
+    });
+});
