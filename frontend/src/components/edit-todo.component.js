16e6c06a3c92d459c8d2b835e2134d8b7313f0de
Prashanth <yvprashanth@gmail.com>
2019-03-25 20:25:54 -0700

    Some more changes

--- frontend/src/components/edit-todo.component.js
+++ frontend/src/components/edit-todo.component.js
@@ -67,7 +67,7 @@ export default class EditTodo extends Component {
             todo_completed: this.state.todo_completed
         };
         console.log(obj);
-        axios.post('http://localhost:4000/todos/update/'+this.props.match.params.id, obj)
+        axios.post('http://localhost:4000/todos/update/' + this.props.match.params.id, obj)
             .then(res => console.log(res.data));
         
         this.props.history.push('/');
